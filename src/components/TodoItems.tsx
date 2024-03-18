ed89c09bf8d19fa15b3bfddd1fefd05b9e6be431
Kian Candelario <kiancandelario1@gmail.com>
2024-03-18 18:31:30 +0800

    style: Improved styling

--- src/components/TodoItems.tsx
+++ src/components/TodoItems.tsx
@@ -9,14 +9,14 @@ type TodoProps = {
 
 const TodoItems = ({ id, title, complete, toggleToDo}: TodoProps) => {
     return ( 
-        <li className="flex gap-1 items-center">
+        <li className="flex gap-1 items-center text-[#352F44] bg-[#B9B4C7] mb-2 p-3">
             <input 
             type="checkbox"
             className="cursor-pointer peer"
             defaultChecked={complete}
             onChange={e => toggleToDo(id, e.target.checked)} 
             />
-            <label htmlFor={id} 
+            <label htmlFor={id}
             className="peer-checked:line-through peer-checked:text-slate-500 cursor-pointer">
                 {title}
             </label>
